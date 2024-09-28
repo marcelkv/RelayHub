9a9839508147454a4fc1031ef66ea63a5bbb3f09
Marcel Kirchenr <marcel_kv@hotmail.com>
2024-09-28 18:35:38 +0200

    Added firebase

---
+++ firebaseConfig.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyALY2Eu62yzZPuaySeDBI3ONz3DYCq2388',
+  authDomain: 'relay-hub.firebaseapp.com',
+  projectId: 'relay-hub',
+  storageBucket: 'relay-hub.appspot.com',
+  messagingSenderId: '1088994606939',
+  appId: '1:1088994606939:web:17dc0c1330726f959ca47e',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
