4fa8060949b7faffa377987e352073ae3467d2c7
Marcel Kirchner <marcel_kv@hotmail.com>
2025-01-06 18:27:58 +0100

    Added filter relays by name

--- src/pages/relays.vue
+++ src/pages/relays.vue
@@ -1,18 +1,25 @@
 <script lang="ts">
-import { ComponentPublicInstance, defineComponent, onMounted, ref } from 'vue';
+import {
+  ComponentPublicInstance,
+  defineComponent,
+  onMounted,
+  ref,
+  watch,
+} from 'vue';
 import Spinner from '../components/spinner.vue';
-import Relay from '../components/relay.vue';
+import RelayComponent from '../components/relay.vue';
 import ButtonDefault from '../components/button-default.vue';
 import { useRelayStore } from '../stores/relay-store';
 import RelayEditable from '../components/relay-editable.vue';
 import PopupAddRelay from '../components/popup-add-relay.vue';
+import { Relay } from '../types/relay';
 
 export default defineComponent({
   components: {
     PopupAddRelay,
     RelayEditable,
     ButtonDefault,
-    Relay,
+    Relay: RelayComponent,
     Spinner,
   },
   emits: ['requestScrollToBottom'],
@@ -22,11 +29,29 @@ export default defineComponent({
     const editableRelayId = ref('');
     const ref_relayItems = ref<(ComponentPublicInstance | null)[]>([]);
     const requestAddNewRelay = ref<boolean>(false);
+    const filterText = ref<string>('');
+    const currentRelays = ref<Relay[]>([]);
 
     onMounted(() => {
       requestAddNewRelay.value = false;
       relayStore.loadRelays();
+      currentRelays.value = relayStore.relays;
+      filterText.value = '';
     });
+
+    function onFilterChanged(): void {
+      const filter = filterText.value.trim().toLowerCase();
+
+      if (filter.length === 0) {
+        currentRelays.value = relayStore.relays;
+      } else {
+        currentRelays.value = relayStore.relays.filter(relay =>
+          relay.name.toLowerCase().includes(filter)
+        );
+      }
+    }
+
+    watch(() => filterText.value, onFilterChanged);
 
     return {
       ref_relayItems,
@@ -34,6 +59,8 @@ export default defineComponent({
       requestAddNewRelay,
       isAddingNewRelay,
       editableRelayId,
+      filterText,
+      currentRelays,
     };
   },
 });
@@ -47,8 +74,14 @@ export default defineComponent({
       v-bind:with-text="true"
     />
     <div v-if="!relayStore.loading && !relayStore.error">
+      <input
+        v-model="filterText"
+        type="text"
+        placeholder="Filter"
+        class="filter"
+      />
       <relay
-        v-for="relay in relayStore.relays"
+        v-for="relay in currentRelays"
         v-bind:key="relay.id"
         v-bind:relay="relay"
       />
@@ -71,5 +104,19 @@ export default defineComponent({
   height: 100%;
   display: flex;
   flex-direction: column;
+
+  input {
+    height: 40px;
+    border: 1px solid lightblue;
+    border-radius: 2px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 20px 5px;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    width: calc(100% - 32px);
+  }
 }
 </style>
